a45bf4dac72ef637882580fed232c9c8566aa685
tobias-fyi <tobyreaper@gmail.com>
2019-10-24 21:18:29 -0600

    Initial layout and styling

--- app.py
+++ app.py
@@ -1,58 +1,43 @@
 import dash
 import dash_bootstrap_components as dbc
 
-"""
-https://github.com/facultyai/dash-bootstrap-components
-
-dash-bootstrap-components provides Bootstrap components.
-
-Plotly Dash is great! However, creating the initial layout can require a lot 
-of boilerplate. dash-bootstrap-components reduces this boilerplate by providing 
-standard layouts and high-level components.
-
-A good way to start customising the stylesheet is to use an alternative 
-pre-compiled theme. Bootswatch is a great place to find new themes. Links to 
-CDNs for each of the Bootswatch styles are also included , and can be used 
-with the external_stylesheets argument of the Dash constructor:
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
-
-Go to https://bootswatch.com/ to preview these Bootswatch themes:
-
-dbc.themes.BOOTSTRAP
-dbc.themes.CERULEAN
-dbc.themes.COSMO
-dbc.themes.CYBORG
-dbc.themes.DARKLY
-dbc.themes.FLATLY
-dbc.themes.JOURNAL
-dbc.themes.LITERA
-dbc.themes.LUMEN
-dbc.themes.LUX
-dbc.themes.MATERIA
-dbc.themes.MINTY
-dbc.themes.PULSE
-dbc.themes.SANDSTONE
-dbc.themes.SIMPLEX
-dbc.themes.SKETCHY
-dbc.themes.SLATE
-dbc.themes.SOLAR
-dbc.themes.SPACELAB
-dbc.themes.SUPERHERO
-dbc.themes.UNITED
-dbc.themes.YETI
-"""
 
 external_stylesheets = [
-    dbc.themes.BOOTSTRAP, # Bootswatch theme
-    'https://use.fontawesome.com/releases/v5.9.0/css/all.css', # for social media icons
+    "/assets/css/main.css",
+    "https://use.fontawesome.com/releases/v5.9.0/css/all.css",  # for social media icons
+    # "/assets/css/bootstrap.css",
+    # dbc.themes.SANDSTONE,  # Bootswatch theme
 ]
 
-meta_tags=[
-    {'name': 'viewport', 'content': 'width=device-width, initial-scale=1'}
-]
+meta_tags = [{"name": "viewport", "content": "width=device-width, initial-scale=1"}]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
 app.config.suppress_callback_exceptions = True
-app.title = 'YOUR APP NAME' # appears in browser title bar
+app.title = "print(fiction)"  # appears in browser title bar
 server = app.server
+
+app.index_string = """
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body class="is-preload">
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            <script src="assets/js/jquery.min.js"></script>
+            <script src="assets/js/browser.min.js"></script>
+            <script src="assets/js/breakpoints.min.js"></script>
+            <script src="assets/js/util.js"></script>
+            <script src="assets/js/main.js"></script>
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+"""
+
--- pages/index.py
+++ pages/index.py
@@ -7,51 +7,122 @@ import plotly.express as px
 
 from app import app
 
-"""
-https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
+header1 = html.Div(
+    [html.H1("Welcome to print(fiction)"), html.P("A predictive modeling project by Tobias Reaper")], id="header"
+)
 
-Layout in Bootstrap is controlled using the grid system. The Bootstrap grid has 
-twelve columns.
-
-There are three main layout components in dash-bootstrap-components: Container, 
-Row, and Col.
-
-The layout of your app should be built as a series of rows of columns.
-
-We set md=4 indicating that on a 'medium' sized or larger screen each column 
-should take up a third of the width. Since we don't specify behaviour on 
-smaller size screens Bootstrap will allow the rows to wrap so as not to squash 
-the content.
-"""
-
-column1 = dbc.Col(
+mainDiv = html.Div(
     [
-        dcc.Markdown(
-            """
-        
-            ## Value Proposition
-
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
-
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
-
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
-
-            """
+        html.Header(
+            [
+                html.H2("Turn the knobs."),
+                html.H2("Try the dials."),
+                html.H2("Slide the sliders."),
+                html.P("I dare you..."),
+            ],
+            className="major container medium",
         ),
-        dcc.Link(dbc.Button('Call To Action', color='primary'), href='/predictions')
+        html.Div(
+            [
+                html.Section(
+                    [
+                        html.A(
+                            [html.Img(src=app.get_asset_url("images/pic01.jpg"))],
+                            href="#",
+                            className="image icon solid fa-signal",
+                        ),
+                        html.Div(
+                            [
+                                html.H3("The First Thing"),
+                                html.P(
+                                    "Vitae natoque dictum etiam semper magnis enim feugiat amet curabitur tempor orci penatibus. Tellus erat mauris ipsum fermentum etiam vivamus eget. Nunc nibh morbi quis fusce lacus."
+                                ),
+                            ],
+                            className="content",
+                        ),
+                    ],
+                    className="feature left",
+                ),
+                html.Section(
+                    [
+                        html.A(
+                            [html.Img(src=app.get_asset_url("images/pic02.jpg"))],
+                            href="#",
+                            className="image icon solid fa-signal",
+                        ),
+                        html.Div(
+                            [
+                                html.H3("The Second Thing"),
+                                html.P(
+                                    "Heirloom farm-to-table XOXO tote bag Marfa deep v kale chips messenger bag distillery Thundercats whatever Neutra dreamcatcher crucifix actually Portland Odd Future ennui bespoke biodiesel."
+                                ),
+                            ],
+                            className="content",
+                        ),
+                    ],
+                    className="feature right",
+                ),
+                html.Section(
+                    [
+                        html.A(
+                            [html.Img(src=app.get_asset_url("images/pic03.jpg"))],
+                            href="#",
+                            className="image icon solid fa-signal",
+                        ),
+                        html.Div(
+                            [
+                                html.H3("The Third Thing"),
+                                html.P(
+                                    "McSweeney's PBR Intelligentsia readymade organic pickled hashtag fanny pack Carles typewriter gastropub cray meh cred post-ironic brunch."
+                                ),
+                            ],
+                            className="content",
+                        ),
+                    ],
+                    className="feature left",
+                ),
+            ],
+            className="box alt container",
+        ),
+        html.Footer(
+            [
+                html.H3("Get shady with data science!"),
+                html.P("See how your prediction stacks up against a machine learning model!"),
+                html.Ul(
+                    [html.Li(html.A("Read like a book", href="#", className="button"))], className="actions special"
+                ),
+            ],
+            className="major container medium",
+        ),
     ],
-    md=4,
+    className="main",
 )
 
-gapminder = px.data.gapminder()
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-           hover_name="country", log_x=True, size_max=60)
+intro = """
+        ## Is It Fiction?
 
-column2 = dbc.Col(
-    [
-        dcc.Graph(figure=fig),
-    ]
-)
+        🤔 Make a prediction if the book title belongs a fiction or nonfiction book.
 
-layout = dbc.Row([column1, column2])
+        🎯 See how your prediction stacks up against the model!
+        """
+
+
+column1 = dbc.Col(header1, width=12)
+
+column2 = dbc.Col(mainDiv, width=12)
+
+# gapminder = px.data.gapminder()
+# fig = px.scatter(
+#     gapminder.query("year==2007"),
+#     x="gdpPercap",
+#     y="lifeExp",
+#     size="pop",
+#     color="continent",
+#     hover_name="country",
+#     log_x=True,
+#     size_max=60,
+# )
+
+# column2 = dbc.Col([dcc.Graph(figure=fig)])
+
+layout = dbc.Container([dbc.Row(column1), dbc.Row(column2)], fluid=True)
