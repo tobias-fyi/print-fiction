7021901fbb5b03399e89cf269b08bb26fbbc4b34
tobias-fyi <tobyreaper@gmail.com>
2020-08-27 20:28:26 -0600

    Cleaned up intro copy and added link to portfolio

--- pages/index.py
+++ pages/index.py
@@ -65,17 +65,8 @@ I am an avid consumer of science-fiction, much of it in book form. Exploring fic
 following the storylines that wind through them play very significant roles in my 
 daily inspiration and keep me full of wonder, even in the real world.
 
-For this project, I was curious how much valuable insight I could gather from books from a quantitative perspective, 
+For this project, I was curious how much valuable insight I could gather from books using a quantitative perspective, 
 as I am usually viewing them from the perspective of being sucked into and being fully immersed by the stories.
-
-It is difficult to encapsulate and measure much the information that is found within the books by 
-the simple means I had available to me while working on this project. However, there are still plenty of 
-interesting avenues to peruse, and I hope you have fun doing so!
-
-Go ahead and tweak the parameters a bit and watch the resulting predictions.
-
-Be on the lookout for any significant separating factors between nonfiction and fiction books; 
-any patterns that the model may have picked up on. They're in there somewhere...
 """
     )
 )
@@ -94,22 +85,39 @@ introduction2 = dbc.Container(
 
 Well...that depends what kind of model you use.
 
-It could be a gradient; the stories we tell or hear others tell are never 100% nonfiction. 
+It could be a gradient; the stories we tell or hear others tell are never 100%
+nonfiction.
 We all like to boost our own narratives just a little bit every now and again.
 
 But in the case of my project, what separates fiction from nonfiction is...
 
 > ...several sliders and a few boolean switches.
 
-I go into more detail about the model, the data, and the process later on. But to give a one-sentence overview, 
-the model I trained for this project is a decision-tree-based machine learning model called a Random Forest Classifier.
+I go into more detail about the data, the model, and the process in my portfolio:
 
-Thanks to the wonders of the Cloud, this model was trained on my laptop, and now lives on one or more servers
-somewhere, happily evaluating the tweaking of its parameters in real time. 
+> [tobias.fyi - print(fiction)](https://tobias.fyi/workshop/printfiction/)
 
-Its entire point of existence?
+To give a brief overview, I built a pipeline that scraped and wrangled book metadata
+from GoodReads, which I used to train and validate a series of machine learning models.
+My baseline was a simple logistic regression model that achieved an F1 score of .65.
+I iterated on the models, engineered some new features and tuned some hyperparameters,
+ultimately getting the best performance/complexity ratio with a random forest, which
+achieved an F1 of about .80.
 
-To provide its best guess at whether those parameters indicate a nonfiction or fiction book.
+Thanks to the wonders of the Cloud, this model was trained on my laptop, and now lives
+on one or more servers somewhere, happily evaluating the tweaking of its parameters in
+real time. 
+
+The entire point of its existence?
+
+To provide its best guess at whether those parameters indicate a nonfiction or fiction
+book.
+
+Go ahead and tweak the input a bit and watch the results. The green text represents the
+model's prediction based on the parameters given to it via the sliders and switches.
+
+Be on the lookout for any significant separating factors between nonfiction and fiction books; 
+any patterns that the model may have picked up on. They're in there somewhere...
 """
     )
 )
@@ -123,7 +131,11 @@ predict_header = dbc.Container(
             dbc.Container(
                 html.Div(
                     html.H3(
-                        ["print(", html.Span(id="nonfiction-pred", style={"color": "#4eb980"}), ")"]
+                        [
+                            "print(",
+                            html.Span(id="nonfiction-pred", style={"color": "#4eb980"}),
+                            ")",
+                        ]
                     )
                 )
             ),
@@ -152,7 +164,9 @@ predict_left = dbc.Col(
             marks={n: str(n) for n in range(1, 6)},
             className="mb-5",
         ),
-        html.Div(id="avg-rating-slider", style={"textAlign": "center"}, className="mb-5"),
+        html.Div(
+            id="avg-rating-slider", style={"textAlign": "center"}, className="mb-5"
+        ),
         dcc.Markdown("""##### **Number of Ratings**"""),
         dcc.Slider(
             id="num-ratings",
@@ -164,7 +178,9 @@ predict_left = dbc.Col(
             marks={n: str(n) for n in range(0, 1000001, 200000)},
             className="mb-5",
         ),
-        html.Div(id="num-ratings-slider", style={"textAlign": "center"}, className="mb-5"),
+        html.Div(
+            id="num-ratings-slider", style={"textAlign": "center"}, className="mb-5"
+        ),
         dcc.Markdown("""##### **Number of Pages**"""),
         dcc.Slider(
             id="num-pages",
@@ -175,7 +191,9 @@ predict_left = dbc.Col(
             marks={n: str(n) for n in range(5, 1006, 100)},
             className="mb-5",
         ),
-        html.Div(id="num-pages-slider", style={"textAlign": "center"}, className="mb-5"),
+        html.Div(
+            id="num-pages-slider", style={"textAlign": "center"}, className="mb-5"
+        ),
     ]
 )
 
@@ -205,7 +223,11 @@ predict_right = dbc.Col(
             marks={n: str(n) for n in range(2, 253, 25)},
             className="mb-5",
         ),
-        html.Div(id="title-char-count-slider", style={"textAlign": "center"}, className="mb-5"),
+        html.Div(
+            id="title-char-count-slider",
+            style={"textAlign": "center"},
+            className="mb-5",
+        ),
         dcc.Markdown("""##### **Publish Year**"""),
         dcc.Slider(
             id="publish-year",
@@ -217,212 +239,10 @@ predict_right = dbc.Col(
             marks={n: str(n) for n in range(1900, 2021, 20)},
             className="mb-5",
         ),
-        html.Div(id="publish-year-slider", style={"textAlign": "center"}, className="mb-5"),
+        html.Div(
+            id="publish-year-slider", style={"textAlign": "center"}, className="mb-5"
+        ),
     ]
-)
-
-# ====== Results Components ====== #
-results_header = dbc.Container(
-    html.Header(
-        [
-            html.Span(className="logo icon solid fa-poo-storm"),
-            html.Div(html.A(html.H2("Data", id="data"), href="#data")),
-            html.Br(),
-        ],
-        className="major container medium",
-    )
-)
-
-results_block_1 = dbc.Container(
-    dcc.Markdown(
-        """
-Much of the up-front work on this project was spent on what I've been calling...
-
-> `target practice` — finding a target that is interesting to predict and is actually predictable. 
-
-This is sometimes much more difficult and time-consuming than one would think. 
-Particularly with the range of data available nowadays, which vary wildly in quality and topic.
-
-I scraped the metadata for ~20k books from a GoodReads list called "Books Everyone Must Read At Least Once, I cleaned it, engineered some new features, built some models, and ultimately this Dash app to take a stab at finding quantitative differences between fiction and nonfiction books
-- As an aspiring data scientist...
-  - Maybe it's somewhat dangerous to venture into such an abstract and philosphical area
-- I'm fascinated by the question of how science-fiction affects the real world
-  - Exploring fictional universes and following the storylines is a very important source of inspiration for me in the real world
-  - I'd love to be able to directly measure that
-
-But I'm practicing my practicality here. It's a practice.
-"""
-    )
-)
-
-results_left = dbc.Container(
-    html.Section(
-        [
-            html.A(
-                html.Img(src="/assets/images/num_ratings_outliers.png", style=img_style),
-                href="#",
-                className="image",
-            ),
-            html.Div(
-                [
-                    html.H3("Outliers, Part 1"),
-                    html.P(
-                        "This data had some very significant outliers that made visualizing relatioships difficult."
-                    ),
-                ],
-                className="content box",
-            ),
-        ],
-        className="feature left",
-    )
-)
-
-results_right = dbc.Container(
-    html.Section(
-        [
-            html.A(
-                html.Img(src="/assets/images/num_reviews_outliers.png", style=img_style),
-                href="#",
-                className="image",
-            ),
-            html.Div(
-                [
-                    html.H3("Outliers, Part 2"),
-                    html.P(
-                        "Filtering out the outliers reduced the size of the dataset by less than 1,000 rows—definitely worth it for the benefit."
-                    ),
-                ],
-                className="content",
-            ),
-        ],
-        className="feature right",
-    )
-)
-
-results_block_2 = dbc.Container(
-    dcc.Markdown(
-        """
-Gathering and fully utilizing the deeper meaning underneath textual content such as books is difficult and 
-requires either a lot of time or a lot of computing power—or both.
-
-> I have neither.
-
-The scope of this project did not extend that far, so I had to get creative with what I had.
-
-For some reason, predicting average ratings wasn't particularly interesting to me. 
-After a few false starts I stumbled onto the idea of trying to predict whether a book is 
-fictional or not, and things fell into place after that.
-
-I engineered a bunch of features in quick succession, most of which I kept after analyzing 
-their permutation importance. 
-"""
-    )
-)
-
-results_footer = (
-    html.Footer(
-        html.Img(src="/assets/images/scatterplot_matrix_no_outliers.png", style=img_style),
-        className="major container medium",
-    ),
-)
-
-
-# ====== Process Components ====== #
-process_header = dbc.Container(
-    html.Header(
-        [
-            html.Span(className="logo icon solid fa-poo-storm"),
-            html.Div(html.A(html.H2("Process", id="process"), href="#process")),
-            html.Br(),
-        ],
-        className="major container medium",
-    )
-)
-
-process_block_1 = dbc.Container(
-    dcc.Markdown(
-        """
-For some reason, predicting average ratings wasn't particularly interesting to me. 
-After a few false starts I stumbled onto the idea of trying to predict whether a 
-book is fictional or not, and things fell into place after that.
-"""
-    )
-)
-
-process_left = dbc.Container(
-    html.Section(
-        [
-            html.A(
-                html.Img(src="/assets/images/scatterplot_matrix_no_outliers.png", style=img_style),
-                href="#",
-                className="image",
-            ),
-            html.Div(
-                [
-                    html.H3("No Outliers"),
-                    html.P(
-                        "Though it's small, without the outliers it is still possible to get a general read."
-                    ),
-                ],
-                className="content",
-            ),
-        ],
-        className="feature left",
-    )
-)
-
-process_right = dbc.Container(
-    html.Section(
-        [
-            html.A(
-                html.Img(src="/assets/images/perm_import_light.png", style=img_style),
-                href="#",
-                className="image",
-            ),
-            html.Div(
-                [
-                    html.H3("Permutation Importance"),
-                    html.P(
-                        "I engineered a bunch of features in quick succession, most of which I kept after analyzing their permutation importance."
-                    ),
-                ],
-                className="content box",
-            ),
-        ],
-        className="feature right",
-    )
-)
-
-# process_scatters = dbc.Container(
-#     html.Div(
-#         html.Img(src="/assets/images/scatterplot_matrix_no_outliers.png")
-#     )
-# )
-
-# process_shap = dbc.Container(
-#     html.Img(src="/assets/images/shap_dark_crop_1.png")
-# )
-
-process_block_2 = dbc.Container(
-    dcc.Markdown(
-        """
-## Choice of model and metrics
-
-Predicting whether a book is fiction or nonfiction is, of course, a binary classification problem. 
-
-There are two possible values True or False, there is _technically_ no in between. 
-But I won't go further into that in this piece.
-
-Logistic regression is not as complex as some of the other classification models, 
-yet can usually provide a robust baseline on which to iterate. 
-
-In my case, that baseline was an accuracy score of ~65%.
-
-For my first attempt to go beyond the baseline, I chose to use a gradient boosting classifier. 
-Due to some dependency issues at the very end, I ended up having to go back 
-and re-train a Random Forest Classifier.
-"""
-    )
 )
 
 
@@ -502,12 +322,16 @@ def num_pages_output(x):
     return f"# Pages: {x}"
 
 
-@app.callback(Output("title-char-count-slider", "children"), [Input("title-char-count", "value")])
+@app.callback(
+    Output("title-char-count-slider", "children"), [Input("title-char-count", "value")]
+)
 def title_char_count_output(x):
     return f"# Characters in Book Title: {x}"
 
 
-@app.callback(Output("publish-year-slider", "children"), [Input("publish-year", "value")])
+@app.callback(
+    Output("publish-year-slider", "children"), [Input("publish-year", "value")]
+)
 def publish_year_output(x):
     return f"Year Published: {x}"
 
